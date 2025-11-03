d8d35a0c23622be632e3173f1af4a0f7a4ebc725
rjongtangco <randolphjoshuaongtangcomuan@gmail.com>
2025-11-04 02:43:46 +0800

    Home page UI

--- trueshot-frontend/src/app/app.routes.ts
+++ trueshot-frontend/src/app/app.routes.ts
@@ -4,6 +4,12 @@ export const routes: Routes = [
   { path: '',
     pathMatch: 'full',
     loadComponent: () => {return import('./home/home').then(m => m.Home)} },
-  { path: 'gallery',
-    loadComponent: () => {return import('./gallery/gallery').then(m => m.Gallery)} }
+  { path: 'catalog',
+    loadComponent: () => {return import('./catalog/catalog').then(m => m.Catalog)} },
+  // { path: 'about',
+  //   loadComponent: () => {return import('./about/about').then(m => m.About)} },
+  { path: 'contact-us',
+    loadComponent: () => {return import('./contact-us/contact-us').then(m => m.ContactUs)} },
+  { path: 'cart',
+    loadComponent: () => {return import('./cart/cart').then(m => m.Cart)} }
 ];
