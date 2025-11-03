f7bc5e819f81ad3d7e45262b8d29f9ac6b583ed2
rjongtangco <randolphjoshuaongtangcomuan@gmail.com>
2025-11-04 03:05:43 +0800

    Contact us

--- trueshot-frontend/src/app/components/featured-cameras/featured-cameras.ts
+++ trueshot-frontend/src/app/components/featured-cameras/featured-cameras.ts
@@ -1,13 +1,10 @@
 import {Component, Input} from '@angular/core';
-import {CommonModule, CurrencyPipe, NgFor, NgIf} from '@angular/common';
-import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-featured-cameras',
-  imports: [CommonModule, NgFor, NgIf, RouterLink, CurrencyPipe],
+  imports: [],
   templateUrl: './featured-cameras.html',
   styleUrl: './featured-cameras.css'
 })
 export class FeaturedCameras {
-
 }
--- trueshot-frontend/src/app/components/footer/footer.ts
+++ trueshot-frontend/src/app/components/footer/footer.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, Inject, PLATFORM_ID} from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {isPlatformBrowser} from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './footer.html',
   styleUrl: './footer.css'
 })
 export class Footer {
   readonly year = new Date().getFullYear();
+
+  private isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) platformId: Object) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
+
+  scrollTop(event: Event) {
+    event.preventDefault();
+    if (this.isBrowser) {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  }
 }
